586a2189219415443b3046a7965d11db6ba06c47
Robert Forbes <robert.forbes@pentlandedge.com>
2017-09-04 09:50:42 +0100

    Added a check for a failed search. Added some comments to the tests.

--- test/egpx_tests.erl
+++ test/egpx_tests.erl
@@ -8,10 +8,15 @@ egpx_test_() ->
 
 find_closest_checks() ->
     {ok, Trackpoints} = egpx:read_file("../test/wiki.gpx"),
+    % Check an empty list of trackpoints produces an error.
+    {error} = egpx:find_closest_trackpoint_time([], {{2009,10,17},{18,37,25,500}}),
+    % Seach before start of trackpoint list.
     {ok, TP1} = egpx:find_closest_trackpoint_time(Trackpoints, {{2009,10,17},{18,37,25,500}}),
     Time1 = egpx:get_time(TP1),    
+    % Seach in between two points. 
     {ok, TP2} = egpx:find_closest_trackpoint_time(Trackpoints, {{2009,10,17},{18,37,30,0}}),
     Time2 = egpx:get_time(TP2),    
+    % Search date beyond end of list but time before first point.
     {ok, TP3} = egpx:find_closest_trackpoint_time(Trackpoints, {{2009,10,18},{18,37,25,500}}),
     Time3 = egpx:get_time(TP3),    
     [?_assertEqual({{2009,10,17},{18,37,26}}, Time1),
